4382333ebfdef3fcbd19ae467348129b4d8cec4d
rodrigo <rob.developer@protonmail.com>
2021-05-13 17:14:46 -0400

    initial commit

---
+++ juego.rb
@@ -0,0 +1,40 @@
+#juego piedra papel o tijera
+
+#Variables de juego
+randGame = rand(0..2)
+userGame = ARGV[0].to_s
+
+#lógica de juego
+#Si juego piedra
+if userGame == "piedra"
+    if randGame == 1
+        puts "Computador juega papel.\nPerdiste."
+    elsif randGame == 2
+        puts "Computador juega tijera.\nGanaste."
+    else
+        puts "Computador juega piedra.\nEmpate."
+    end
+#Si juego papel
+elsif userGame == "papel"
+    if randGame == 0
+        puts "Computador juega piedra.\nGanaste."
+    elsif randGame == 2
+        puts "Computador juega tijera.\nPerdiste."
+    else
+        puts "Computador juega papel.\nEmpate."
+    end
+#Si juego tijera
+elsif userGame == "tijera"
+        if randGame == 0
+            puts "Computador juega piedra.\nPerdiste."
+        elsif randGame == 1
+            puts "Computador juega papel.\nGanaste."
+        else
+            puts "Computador juega tijera.\nEmpate."
+        end
+#Validación de datos de entrada
+else 
+    puts "Argumento inválido: Debe ser piedra, papel o tijera."
+end
+
+
